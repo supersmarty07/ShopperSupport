95ce09ea106bba6936a4c9269dc77e7bcaa1189e
supersmarty07 <elohistsachet007@gmail.com>
2021-09-18 18:56:12 +1000

    Added Disclaimer

--- src/Header/Footer.jsx
+++ src/Header/Footer.jsx
@@ -3,7 +3,25 @@ import "./Header.css";
 function Footer() {
   return (
     <div className="center">
-      <h5>Developed by Sachet </h5>
+      <h5>
+        Developed by Sachet <br />
+        Front-end Built with React || API built with NodeJs & hosted on Heroku
+        || Testing with Cypress & Deployed on Netlify
+        <br />
+        <a
+          href="https://github.com/supersmarty07/OnlineShopperSupport"
+          target="_blank"
+        >
+          Go to Code
+        </a>
+      </h5>
+      <h6>
+        Fair Use Notice
+        <br />
+        This application doesnot contain any copyrighted materials.
+        <br />
+        Please contact elohistsachet07@gmail.com
+      </h6>
     </div>
   );
 }
